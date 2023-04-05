6081266462c033b33227fd0ce56f431c09856159
adarshkannada <adarshkannada@outlook.com>
2023-04-06 00:08:15 +0530

    Update comprehensions.py

--- python/basic_programs/comprehensions.py
+++ python/basic_programs/comprehensions.py
@@ -12,3 +12,12 @@ listb = [2, 3, 5, 7, 11]
 squared_list = [x**2 for x in listb if x%2 == 0]
 print(squared_list)
 
+# Combining multiple lists into one
+print("-------------")
+a = [1, 2, 3, 5]
+b = ['a', 'c', 'g']
+combined = [(str(x)+y) for (x,y) in zip(a,b)]
+# nested iterator
+combined_inner = [(x,y) for x in a for y in b]
+print(combined)
+print(combined_inner)
